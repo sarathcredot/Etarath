9b6285dd667f44fda378402fc3151a256e5c6ac2
Sanin Credot <sanin@credot.in>
2025-08-03 12:42:44 +0530

    updated heading 7 component

--- src/components/common/Heading7.tsx
+++ src/components/common/Heading7.tsx
@@ -7,6 +7,6 @@ type Props = {
 
 const Heading7 = ({ className = "", children }: Props) => {
   return <h4 className={` text-[20px] md:text-[22px] lg:text-[24px] font-[600] capitalize   ${className}`}>{children}</h4>;
-};
+};   
 
 export default Heading7;
