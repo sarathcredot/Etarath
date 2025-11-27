dc0176a23339ae3a8cff3ef12a1a5135cea7f44f
sarathcredot <sarath@credot.in>
2025-11-27 14:31:13 +0530

    navbar link updated

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -96,13 +96,13 @@ const Footer = () => {
             />
           </Link>
           <div className="md:absolute left-1/2 top-1/2 md:-translate-x-1/2 md:-translate-y-1/2   flex items-center justify-center gap-7 lg:gap-10 text-[16px] font-[600] rounded-full   py-[15px] transition-all duration-300 ease-in-out ">
-            <Link href={"/pricing"} className="">
+            <Link href={"/#"} className="">
               Pricings
             </Link>
-            <Link href={"/blog"} className="">
+            <Link href={"/#"} className="">
               Blog
             </Link>
-            <Link href={"/contact"} className="">
+            <Link href={"/#"} className="">
               Contact
             </Link>
           </div>
