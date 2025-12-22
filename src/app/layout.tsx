b6c6ab436211a6cda7ccaa44d43ec9c8e2c32121
sarathcredot <sarath@credot.in>
2025-12-22 16:56:35 +0530

    meta data updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
+
+
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import { Plus_Jakarta_Sans, Open_Sans, Manrope } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
-// Plus Jakarta Sans
 const plusJakarta = Plus_Jakarta_Sans({
-  variable: "--font-plus-jakarta", // CSS variable
+  variable: "--font-plus-jakarta",
   subsets: ["latin"],
   display: "swap",
 });
 
-// Open Sans
 const openSans = Open_Sans({
   variable: "--font-open-sans",
   subsets: ["latin"],
@@ -20,44 +19,44 @@ const openSans = Open_Sans({
 });
 
 const manrope = Manrope({
-  variable: "--font-manrope", // CSS variable for Manrope
+  variable: "--font-manrope",
   subsets: ["latin"],
   display: "swap",
 });
 
-
 export const metadata: Metadata = {
+  metadataBase: new URL("https://etarath.ai"), // 🔥 REQUIRED
   title: "The UAE’s Ultimate B2B Tyre Marketplace",
   description: "Connecting verified vendors and retailers with speed, trust, and intelligence.",
   icons: {
-    icon: "/etarath_favicon.svg", // Path to your favicon
+    icon: "/etarath_favicon.svg",
   },
   openGraph: {
     title: "The UAE’s Ultimate B2B Tyre Marketplace",
     description: "Connecting verified vendors and retailers with speed, trust, and intelligence.",
-    images: ["/etarath_black.png"], // Meta image (large)
+    images: ["/etarath_black.png"],
   },
   twitter: {
     card: "summary_large_image",
-    images: ["/etarath_black.png"], // Meta image (large)
+    images: ["/etarath_black.png"],
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${plusJakarta.variable} ${openSans.variable} ${manrope.variable} antialiased`}
       >
         <Navbar />
-        {/* Main content */}
-        <div className="mt-[80px] px-5 md:px-10 lg:px-[70px] max-w-[1480px] mx-auto">{children}</div>
+        <div className="mt-[80px] px-5 md:px-10 lg:px-[70px] max-w-[1480px] mx-auto">
+          {children}
+        </div>
         <Footer />
-
       </body>
     </html>
   );
