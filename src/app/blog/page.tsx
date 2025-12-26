eb0443754aa1b9eb70972419f7570c2b7bd5d217
sarathcredot <sarath@credot.in>
2025-12-26 19:57:43 +0530

    blog page changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,36 @@
+"use client";
 import React from 'react'
 import BlogBanner from '@/components/blog/Banner'
 import Blogs from '@/components/blog/Blogs'
+import { useState } from 'react';
+import Paragraph from '@/components/common/Paragraph';
 
 function Blog() {
+
+    const [show, setShow] = useState(true)
+
     return (
         <div>
 
-            <BlogBanner />
-            <Blogs />
-           
+            {
+                show ?
+                    <>
+                        <BlogBanner />
+                        <Blogs />
+                    </>
+
+                    :
+                    <div className="text-center mb-20">
+                        <Paragraph className="text-lg font-medium mb-2">
+                            No blogs available
+                        </Paragraph>
+                        <Paragraph className="opacity-70 text-sm">
+                            New articles will be published soon. Stay tuned.
+                        </Paragraph>
+                    </div>
+            }
+
+
 
         </div>
     )
